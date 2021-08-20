87c848c25481fcadf55833239a962cda16487836
Heather OBrien <haobrientx@gmail.com>
2021-08-20 17:44:40 -0500

    Updates

---
+++ precache-manifest.ba40020a7783dcbe09d28333201b7407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d960e7ab7eefd4462ca8",
+    "url": "/lights-out/static/css/main.67a9913b.chunk.css"
+  },
+  {
+    "revision": "d960e7ab7eefd4462ca8",
+    "url": "/lights-out/static/js/main.d960e7ab.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights-out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "d5335d2d719f08a3196faf0bc5071025",
+    "url": "/lights-out/index.html"
+  }
+];
